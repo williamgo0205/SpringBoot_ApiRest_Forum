8a1a3a2b670e866ee475e3654bc75631c1def6d6
William Gonçalves de Oliveira <william.oliveira@matera.com>
2020-10-15 20:08:48 -0300

    Adicionando arquivos treinamento Spring boot 15-10-2020 autenticação token

--- src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
+++ src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
@@ -1,5 +1,6 @@
 package br.com.alura.forum.config.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -7,14 +8,20 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfigurations extends WebSecurityConfigurerAdapter{
 	
+	@Autowired
+	private AutenticacaoService autenticacaoService;
+	
 	// Configurações de autenticação
 	@Override 
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(autenticacaoService).passwordEncoder(new BCryptPasswordEncoder()); // encripitação da senha
 	}
 	
 	// Configurações de Autorização (URL's do Projeto)
@@ -23,12 +30,24 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter{
 		// Libera acesso à requisição nesse caso Método GET do /topicos (Lista e detalhes do tópico)
 		http.authorizeRequests()
 		  .antMatchers(HttpMethod.GET, "/topicos").permitAll() 
-		  .antMatchers(HttpMethod.GET, "/topicos/*").permitAll();
+		  .antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
+		  .antMatchers(HttpMethod.POST, "/auth").permitAll()
+		  .anyRequest().authenticated() // Somente user autenticados
+		  //.and().formLogin() // Formulário padrão de Login
+		  .and().csrf().disable() //Autenticação via token
+		  .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // Não cria sessão e utiliza token de maneira stateless, mas não tem mais formulário
+		  ;
 	}
 	
 	// Configurações de Recusros Estáticos (Js, css, imagens)
 	@Override
 	public void configure(WebSecurity web) {
+		
 	}
+	
+	// Gera senha cripitografada
+//	public static void main(String[] args) {
+//		System.out.println(new BCryptPasswordEncoder().encode("123456"));
+//	}
 	
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$.X33vm6zf93SZksdhOkRUuhbT8z60BIZz4LGmhg/K4axI5jqfUmua');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
